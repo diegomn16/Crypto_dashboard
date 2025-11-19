c860a42ed4f05c2d834e46180fc018fc4b9ef2a8
Diego <diegomn2006@gmail.com>
2025-11-19 19:10:01 +0100

    Adición de script para cálculo de métricas de las crypto para crear gráficas en el futuro

---
+++ src/features.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+df = pd.read_csv('data/processed/Joint_data.csv', parse_dates=['date'])
+df = df.set_index('date')
+df.columns = df.columns.str.replace('_close$', '', regex=True)
+
+def calculate_drawdown(series):
+    peak = series.cummax()
+    drawdown = (series / peak) - 1
+    return drawdown
+
+#standard price calculation
+df_norm = (df/df.iloc[0]).add_suffix('_norm')
+
+#calculation of daily returns
+df_return = df.pct_change().add_suffix('_return').fillna(0)
+
+#calculation of cumulative returns
+df_cumreturn = ((df_return+1).cumprod() - 1).fillna(0)
+df_cumreturn.columns = df_cumreturn.columns.str.replace('_return$', '_cumreturn', regex = True)
+
+#rolling volatility calculation
+df_vol_roll = df_return.rolling(window=30).std().fillna(0)
+df_vol_roll.columns = df_vol_roll.columns.str.replace('_return$', '_vol_roll', regex=True)
+
+#drawdown
+df_drawdown = df_norm.apply(calculate_drawdown, axis = 0)
+df_drawdown.columns = df_drawdown.columns.str.replace('_norm$', '_drawdown', regex=True)
+
+df = df.join([df_norm, df_return, df_cumreturn, df_vol_roll, df_drawdown])
+df.to_csv('data/processed/Joint_features.csv')
